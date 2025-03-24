e0ded928c5f3fe992101d95cda73732b0506408d
nitinkumargorai <nitinkumargorai2004@gamil.com>
2025-03-24 16:05:02 +0530

    Nothing

---
+++ variable and data types/variable.py
@@ -0,0 +1,5 @@
+a = input("What is Your Name? ")
+b = input("What is Your Age? ")
+c = input("What is Your Favorite Color? ")
+
+print("Hlooo", a, "You Said That You're", b, "Years Old.\n Your Favorite Color is", c)
