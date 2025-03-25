432e32321bca5ea3c534e1f1778a5462303f89e6
nitinkumargorai <nitinkumargorai2004@gamil.com>
2025-03-25 10:34:35 +0530

    Nothing

---
+++ variable and data types/calculation.py
@@ -0,0 +1,13 @@
+#sum
+a = 2
+b = 5
+sum = a+b
+print(sum)
+#we also calculate this using this method
+
+# print(a+b)
+ 
+#difference
+dif = a-b
+print(dif)
+
---
+++ variable and data types/data_types.py
@@ -0,0 +1,12 @@
+Age = 23
+Name = "Nitin"
+old = True
+Remarks = None
+#Now we are printing the data types using type method
+
+print(type(Age))
+print(type(Name))
+print(type(old))
+print(type(Remarks))
+
+#When we type the boolean value (True/False) we must ensure that the start letter is always in capital letter
