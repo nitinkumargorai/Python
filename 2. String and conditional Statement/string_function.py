6c0b782f91402e9d7c05c43c511c1f80217705a2
nitinkumargorai <nitinkumargorai2004@gamil.com>
2025-03-26 23:56:59 +0530

    Completing The String and conditional Statements

---
+++ 2. String and conditional Statement/string_function.py
@@ -0,0 +1,13 @@
+#String Functions
+str = "i'm Learning Python from Apna College"
+print(str.endswith("ege")) #True
+#capatalize
+print(str.capitalize()) #it works only once
+print(str) #original String will shows 
+
+# replace
+print(str.replace("Python", "Javascript"))
+
+print(str.find("o")) #it shows first index of first occurrer
+print(str.count("o"))
+#funtion is very powerfull things in our python programming
