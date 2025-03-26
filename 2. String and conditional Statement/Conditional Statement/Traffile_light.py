ac97437540ca4511dda8c5b931746eefc2a3436a
nitinkumargorai <nitinkumargorai2004@gamil.com>
2025-03-27 00:45:34 +0530

    Completing Practice set of conditional statement

---
+++ 2. String and conditional Statement/Conditional Statement/Traffile_light.py
@@ -0,0 +1,11 @@
+light = input("Enter the color of the light:")
+if(light == "red"):
+    print("Plese Stop")
+elif(light == "yellow"):
+    print("Go slowly")
+elif(light == "green"):
+    print("Can Go")
+else:
+    print("Light is Broken,,, Please inform to the traffic police")
+
+    print(light)
